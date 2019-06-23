789e74d92642f39e11cee4a5c294d4e49ecaac28
Audra Tallman <audi.tallman@gmail.com>
2019-06-23 20:59:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ function writeCards(names, event) {
   for (let i = 0; i < names.length; i++) {
     thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
- 
   return thankYouCards;
+}
+
+function countdown(countdown) {
+  while (countdown > 0) {
+    console.log(countdown);
+    countdown -=1;
+  }
+  console.log(countdown);
 }
