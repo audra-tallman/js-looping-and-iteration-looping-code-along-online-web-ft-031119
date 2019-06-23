2d87724139a9c9734031e97dfa36827af40c4d3f
Audra Tallman <audi.tallman@gmail.com>
2019-06-23 20:51:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const names = ["Ada", "Brendan", "Ali"];
-const event  = ["birthday"];
- 
 function writeCards(names, event) {
   for (let i = 0; i < names.length; i++) {
     console.log(`Thank you, ${names[i]} for  the wonderful ${event}!`);
