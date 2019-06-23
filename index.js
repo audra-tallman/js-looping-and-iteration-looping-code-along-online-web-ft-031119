f8e40359c05d55372f95e348623e65cd9b0e7bab
Audra Tallman <audi.tallman@gmail.com>
2019-06-23 20:55:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function writeCards(names, event) {
-  let thankYouCards = []
+  let thankYouCards = [];
   for (let i = 0; i < names.length; i++) {
     thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
